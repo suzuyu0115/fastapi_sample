aab2130b81e8459728b9e5db87e1141a986fd32d
suzuyu0115 <szkyut0115@gmail.com>
2023-12-06 17:21:08 +0900

    Add: schema-request

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -2,7 +2,21 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
-class Task(BaseModel):
-  id: int
+class TaskBase(BaseModel):
   title: Optional[str] = Field(None, example = "クリーニングを取りに行く")
-  done: bool = Field(False, description = "完了フラグ")
+
+class TaskCreate(TaskBase):
+  pass
+
+class TaskCreateResponse(TaskCreate):
+  id: int
+
+  class Config:
+    orm_mode = True
+
+class Task(TaskBase):
+  id: int
+  done: bool = Field(False, desctiption = "完了フラグ")
+
+  class Config:
+    orm_mode = True
